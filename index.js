99ce271fe812d2d09db09d07131a574dc2bc548a
Jakob Viktor Albrechtsen <jakobvial@gmail.com>
2022-09-18 16:14:42 +0200

    End lesson 185

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-$(document).keydown(function (event) {
-    $("h1").text(event.key);
-});
-
-$("h1").on("mouseover", function () {
-    $("h1").css("color", "purple");
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
 });
